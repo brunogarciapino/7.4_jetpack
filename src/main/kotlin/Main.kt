bc75961d87a88c8e06e88bb1c24bb15578973d4a
Bruno <brunogarciapino@gmail.com>
2022-03-16 14:25:50 +0100

    Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,30 +2,37 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 @Preview
 fun App() {
 
     MaterialTheme {
+
+        val fondo1 by remember { mutableStateOf(setOf("Imagenfondo2.jpg","R.jpg","A.png").random()) }
+        var isDialogOpen2 by remember {mutableStateOf(false)}
+        var isDialogOpen by remember {mutableStateOf(false)}
         var contador by remember { mutableStateOf(0) }
-        var color by remember { mutableStateOf(androidx.compose.ui.graphics.Color.White) }
+        var color by remember { mutableStateOf(Color.White) }
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = color
@@ -39,43 +46,60 @@ fun App() {
                 verticalAlignment = Alignment.Top,
                 horizontalArrangement = Arrangement.Start
             ) {
-                IconButton(onClick =) {
-                    Icon(
-                        painter = painterResource("jet.png"),
-                        contentDescription = null
-                    )
-                }
 
+                    }
                 Column(
                     modifier = Modifier.fillMaxHeight().fillMaxWidth(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+                    if (isDialogOpen) { //Esto solo se activara cuando isDialogOpen sea true, el cual abrira una ventana donde se mostrara el texto oculto
+                        Dialog(
+                            onCloseRequest = { isDialogOpen = false },
+                            onPreviewKeyEvent = {
+                                if (it.key == Key.Escape && it.type == KeyEventType.KeyDown) {
+                                    isDialogOpen = false
+                                    true
+                                } else {
+                                    false
+                                }
+                            }) {
+                            Image(
+                                painter = painterResource(fondo1),
+                                contentDescription = "fondo"
+                            )
+
+                        }
+                    }
 
                     Button(onClick = {
                         contador++
                         color = setOf(
-                            androidx.compose.ui.graphics.Color.Blue,
-                            androidx.compose.ui.graphics.Color.Red,
-                            androidx.compose.ui.graphics.Color.Green,
-                            androidx.compose.ui.graphics.Color.Black,
-                            androidx.compose.ui.graphics.Color.Gray,
-                            androidx.compose.ui.graphics.Color.Magenta,
-                            androidx.compose.ui.graphics.Color.Yellow,
-                            androidx.compose.ui.graphics.Color.DarkGray,
-                            androidx.compose.ui.graphics.Color.White,
-                            androidx.compose.ui.graphics.Color.Cyan,
+                            Color.Blue,
+                            Color.Red,
+                            Color.Green,
+                            Color.Black,
+                            Color.Gray,
+                            Color.Magenta,
+                            Color.Yellow,
+                            Color.DarkGray,
+                            Color.White,
+                            Color.Cyan,
                         ).random()
 
+                        if (contador == 20)  {
+                         isDialogOpen= true
+                        }
+
                     }) {
-                        Text(text = "Click ", color = androidx.compose.ui.graphics.Color.Magenta)
-                        Text(text = "${contador}", color = androidx.compose.ui.graphics.Color.Green)
+                        Text(text = "Click ", color = Color.Magenta)
+                        Text(text = "${contador}", color = Color.Green)
                     }
                 }
             }
         }
     }
-}
+
 
 
 fun main() = application {
